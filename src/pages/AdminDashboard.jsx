63f8c34348ddcfecf3dc366e97a7510a219da2da
athleticmeet-ptu <athleticptu@gmail.com>
2025-09-04 19:46:54 +0530

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -306,13 +306,7 @@ const genderData = React.useMemo(() => {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-64">
-        <motion.div
-          animate={{ rotate: 360 }}
-          transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
-          className="w-8 h-8 border-4 border-primary border-t-transparent rounded-full"
-        />
-      </div>
+     <div className="w-12 h-12 border-4 border-orange-500 border-dashed rounded-full animate-spin"></div>
     );
   }
 
